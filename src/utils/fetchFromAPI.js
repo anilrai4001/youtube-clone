dd5ef794e1ac0d40291b62d847e9ea6315cdd6b2
Anil Rai <anilrai4001@gmail.com>
2023-09-26 07:44:04 +0530

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -7,7 +7,7 @@ const options = {
     maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': 'd6d5fcc08fmsh15ce32c3f0c93e8p1fb5cfjsnace0a4ebca8b',
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
 };
